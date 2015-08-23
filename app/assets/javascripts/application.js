d089aa8a6019aa1aa0bef91fef1afca795b251ac
billupillu <billu.pillu123@gmail.com>
2015-08-23 23:13:47 +0530

    Inital Commit sumi

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,44 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+
+//= require turbolinks
+//= require_tree .
+
+$(document).ready(function(){
+	$('.h').hide();
+	$('.i').hide();
+	$('.c').hide();
+	$('.k').hide();
+	$('.b').hide();
+	$('.j').hide();
+	$('.z').hide();
+	$('.feedback-button').click(function(){
+		$('.i').fadeToggle("slow",null,function(){
+			$('.h').fadeToggle("slow",null, function(){
+				$('.b').fadeToggle("slow",null, function(){
+					$('.c').fadeToggle("slow",null,function(){
+						$('.k').fadeToggle("slow",null,function(){
+							$('.z').fadeToggle("slow",null,function(){
+								$('.j').fadeToggle("slow",null,function(){});
+							});
+						});
+					});
+				});
+			});
+		});
+
+	});
+});
